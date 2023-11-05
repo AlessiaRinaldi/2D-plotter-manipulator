e4ecd4a3214845f34d5b32d78513fe2595a1861f
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2023-11-05 17:44:38 +0100

    motors: wrong way, check dc manipulation

--- MSP-codes/src/motors.c
+++ MSP-codes/src/motors.c
@@ -1,5 +1,28 @@
 #include "motors.h"
+#include "stdio.h"
 
+void init_timer(void){
+
+    MAP_WDT_A_holdTimer();                                      // waiting WDT
+    MAP_Interrupt_disableMaster();                              // disabling master interrupts
+
+    TIMER_A0->CCR[0] = 60000 - 1;                               /* PWM Period (3Mhz/60k=50hz) */
+    TIMER_A0->CCTL[4] = 0xE0;                                   /* CCR4 reset/set mode */
+    TIMER_A0->CTL = 0x0214;                                     /* use SMCLK, count up, clear TA0R register */
+}
+
+void move_servo(float angle){
+    if(angle >= 0 && angle <= 180){
+        int duty_cycle = angle / 180 * (7500 - 1500) + 1500;
+        TIMER_A0 -> CCR[4] = duty_cycle;
+
+    } else{
+        printf("Wrong angle\n");
+    }
+}
+
+
+/*
 void move_servo(uint16_t dutyCycle){
     // dutyCycle = SERVO_DUTY_CYCLE_MIN;                      // Starting duty cicle for the servo --> starting position
     // TODO: check dutycycle validity
@@ -23,3 +46,4 @@ void move_servo(uint16_t dutyCycle){
         }
     }
 }
+*/
--- MSP-codes/src/motors.h
+++ MSP-codes/src/motors.h
@@ -11,7 +11,10 @@ This file include the header for the management of the Servo motors used for the
 #define PWM_FREQUENCY 50                                            // PWM frequency, check
 #define SERVO_DUTY_CYCLE_MIN 3200                                   // Minimum duty cicle means minimum position
 #define SERVO_DUTY_CYCLE_MAX 6400                                   // Maximum duty cicle means maximum position
+#define CLK_FREQ 3000000
 
-void move_servo(uint16_t dutyCycle);
+void PORT1_IRQHandler(void);
+void init_timer(void);
+void move_servo(float angle);
 
 #endif
