35d53dddc464b4d89e68e1f9f05e357abafc7a1d
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2023-11-20 19:35:31 +0100

    motor: change approach to upconfig

--- MSP-codes/src/motors.c
+++ MSP-codes/src/motors.c
@@ -6,24 +6,20 @@
 Set pwmconfig structs
 */
 
-Timer_A_PWMConfig pwmConfig0 =
-{
-        TIMER_A_CLOCKSOURCE_SMCLK,
-        TIMER_A_CLOCKSOURCE_DIVIDER_1,
-        2000, // this num/ 64k = period       OG 32000
-        TIMER_A_CAPTURECOMPARE_REGISTER_1,
-        TIMER_A_OUTPUTMODE_RESET_SET,
-        64 // 5% or 1ms duty cycle             OG 3200
+Timer_A_CompareModeConfig compareConfig_PWM = {
+        TIMER_A_CAPTURECOMPARE_REGISTER_3,          // Use CCR3
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_DISABLE,   // Disable CCR interrupt
+        TIMER_A_OUTPUTMODE_RESET_SET,               // Toggle output b
+        500,                                        // starting Duty Cycle
 };
 
-Timer_A_PWMConfig pwmConfig2 =
-{
-        TIMER_A_CLOCKSOURCE_SMCLK,
-        TIMER_A_CLOCKSOURCE_DIVIDER_1,
-        2000, // this num/ 64k = period       OG 32000
-        TIMER_A_CAPTURECOMPARE_REGISTER_1,
-        TIMER_A_OUTPUTMODE_RESET_SET,
-        64 // 5% or 1ms duty cycle             OG 3200
+const Timer_A_UpModeConfig upConfig = {
+        TIMER_A_CLOCKSOURCE_SMCLK,                  // SMCLK = 3 MhZ
+        TIMER_A_CLOCKSOURCE_DIVIDER_1,              // SMCLK/12 = 250 KhZ
+        10000,                                      // 40 ms tick period
+        TIMER_A_TAIE_INTERRUPT_DISABLE,             // Disable Timer interrupt
+        TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,        // Disable CCR0 interrupt
+        TIMER_A_DO_CLEAR                            // Clear value
 };
 
 /*
@@ -33,18 +29,38 @@ pos_t current_position;
 
 
 void init_servo(void){
+    /* Configures P2.4 to PM_TA0.1 for using Timer PWM to control LED */
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4,
+    GPIO_PRIMARY_MODULE_FUNCTION);
 
-    /*
-    Set up pwm pins module function
-    */
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN4,GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P5, GPIO_PIN6,GPIO_PRIMARY_MODULE_FUNCTION);
+    /* Configures P2.6 to PM_TA0.3 for using Timer PWM to control LED */
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN6,
+    GPIO_PRIMARY_MODULE_FUNCTION);
 
-    /*
-    Set starting pwm configuration
-    */
-    MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig0);
-    MAP_Timer_A_generatePWM(TIMER_A2_BASE, &pwmConfig2);
+    /* Configures P5.6 to PM_TA0.4 for using Timer PWM to control LED */
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P5, GPIO_PIN6,
+    GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Configuring Timer_A0 for Up Mode and starting */
+    Timer_A_configureUpMode(TIMER_A0_BASE, &upConfig);
+    Timer_A_startCounter(TIMER_A0_BASE, TIMER_A_UP_MODE);
+
+    /* Configuring Timer_A2 for Up Mode and starting */
+    Timer_A_configureUpMode(TIMER_A2_BASE, &upConfig);
+    Timer_A_startCounter(TIMER_A2_BASE, TIMER_A_UP_MODE);
+
+    /* Initialize compare registers to generate PWM */
+    Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM); // For P2.6
+
+    // For Port 2.4
+    compareConfig_PWM.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
+    compareConfig_PWM.compareValue = 3000;
+    Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM);
+
+    // For Port 5.6
+    compareConfig_PWM.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
+    compareConfig_PWM.compareValue = 9000;
+    Timer_A_initCompare(TIMER_A2_BASE, &compareConfig_PWM);
 }
 
 uint16_t angle_2_duty(uint16_t angle){
@@ -60,8 +76,7 @@ void set_servo(uint16_t duty1, uint16_t duty2){
     */   
     if(duty1 >= SERVO_DUTY_CYCLE_MIN && duty1 <= SERVO_DUTY_CYCLE_MAX && duty2 >= SERVO_DUTY_CYCLE_MIN && duty2 <= SERVO_DUTY_CYCLE_MAX){
         
-        MAP_Timer_A_generatePWM(TIMER_A0_BASE, &pwmConfig0);
-        MAP_Timer_A_generatePWM(TIMER_A2_BASE, &pwmConfig2);
+        
     } 
 }
 
