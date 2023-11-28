f69a283a6ffdb7a65216f777f738783cd3262ffe
Angelo Nutu <nutuangelo@yahoo.it>
2023-11-28 13:40:15 +0100

    screen: screen function first version

--- MSP-codes/src/screen.c
+++ MSP-codes/src/screen.c
@@ -21,6 +21,9 @@ void init_screen(void){
     Graphics_setBackgroundColor(&context, bgColor);
     Graphics_setFont(&context, &font);
 
+    vectorsDrawn = 0;
+    isDrawing = false;
+    prevPercentage = 0;
     /*
     // Bound to be removed, for testing of font / colors / proportions
     Graphics_drawStringCentered(&context, (int8_t *) "Silent Leges", AUTO_STRING_LENGTH, 64, 64 - (int32_t) Graphics_getFontHeight(font) / 2, OPAQUE_TEXT);
@@ -37,8 +40,13 @@ void drawDefault(void){
     Graphics_flushBuffer(&context);
 
     // Draws default booting image
-   Graphics_drawImage(&context, &Default, 0, 0);
-   drawInterface();
+    Graphics_drawImage(&context, &Default, 0, 0);
+    int i;
+    for(i = 0; i < 1000; i++){
+        __delay_cycles(10000);
+        updateScreen();
+    }
+   
 }
 
 // DEMO: needs to be changed
@@ -46,15 +54,31 @@ void drawInterface(void){
     Graphics_clearDisplay(&context);
     Graphics_flushBuffer(&context);
     const Graphics_Rectangle motor1 = {10, 10, 30, 30};
-    const Graphics_Rectangle motor2 = {10, 50, 30, 70};
+    const Graphics_Rectangle motor2 = {74, 10, 94, 30};
     Graphics_fillRectangle(&context, &motor1);
     Graphics_drawStringCentered(&context, (int8_t*) ":", AUTO_STRING_LENGTH, 40, 18, OPAQUE_TEXT);
     Graphics_fillRectangle(&context, &motor2);
-    Graphics_drawStringCentered(&context, (int8_t*) ":", AUTO_STRING_LENGTH, 40, 58, OPAQUE_TEXT);
+    Graphics_drawStringCentered(&context, (int8_t*) ":", AUTO_STRING_LENGTH, 104, 18, OPAQUE_TEXT);
     Graphics_drawImage(&context, &DrawingBar, 0, 65);
-    Graphics_drawStringCentered(&context, (int8_t*) "0", AUTO_STRING_LENGTH, 64, 115, OPAQUE_TEXT);
+    //Graphics_drawStringCentered(&context, &vectorsDrawn, AUTO_STRING_LENGTH, 64, 115, OPAQUE_TEXT);
 }
 
 void updateScreen(void){
-    int numVec = 1000; // dummy num of vectors
+    if(!isDrawing){
+        isDrawing = true;
+        drawInterface();
+    }
+    uint8_t startX = 8;
+    uint8_t startY = 64 + 23;
+    uint16_t numVec = 1000; // dummy num of vectors
+    vectorsDrawn++;
+    uint8_t percentage = floor(vectorsDrawn * 100 / numVec);
+    int8_t buffer[5];
+    snprintf((char*)buffer, sizeof(buffer), "%u", percentage);
+    Graphics_Rectangle rect = {0, 105, 70, 128};
+    if(prevPercentage != percentage) {
+        prevPercentage = percentage;
+        Graphics_fillRectangleOnDisplay(&g_sCrystalfontz128x128, &rect, 0xFFFF);
+    }
+    Graphics_drawStringCentered(&context, &buffer[0], AUTO_STRING_LENGTH, 60, 115, OPAQUE_TEXT);
 }
--- MSP-codes/src/screen.h
+++ MSP-codes/src/screen.h
@@ -5,17 +5,21 @@ This file include the header for the management of the on-screen feedback
 #ifndef __SCREEN_H__
 #define __SCREEN_H__
 
+#include <stdio.h>
 #include <msp.h>
 #include <grlib.h>
+#include <math.h>
 #include <Crystalfontz128x128_ST7735.h>
 #include <HAL_MSP_EXP432P401R_Crystalfontz128x128_ST7735.h>
 
-#define bgColor 0x00000000      // background color, CURRENT: Black
-#define fgColor 0x00DAA520      // foreground color, CURRENT: Golderon
+#define bgColor 0xffffffff      // background color, CURRENT: Black
+#define fgColor GRAPHICS_COLOR_BLUE_VIOLET      // foreground color, CURRENT: Golderon
 #define font    g_sFontCmss18b  // font type, CURRENT: Cmss 18px Bold
 
 Graphics_Context context; // Screen context used for all feedback
-//uint16_t vectorsDrawn = 0; // Number of vectors drawn, updated each time function "updateScreen" is called
+uint16_t vectorsDrawn; // Number of vectors drawn, updated each time function "updateScreen" is called
+bool isDrawing; 
+uint8_t prevPercentage;
 
 void init_screen(void);   /* Basic screen setup */
 void drawDefault(void);   /* Draws default screen */
