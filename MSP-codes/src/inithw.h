ea3a7650bf8c0e6b3de289540dec510068882184
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2023-12-13 12:07:17 +0100

    motor/inithw: add more info

--- MSP-codes/src/inithw.h
+++ MSP-codes/src/inithw.h
@@ -1,6 +1,9 @@
 #ifndef __INITHW_H__
 #define __INITHW_H__
 
+/**
+ * @brief       Overall function that init motors, communication and screen
+*/
 void inithw();
 
 #endif
--- MSP-codes/src/motors.h
+++ MSP-codes/src/motors.h
@@ -14,10 +14,34 @@ This file include the header for the management of the Servo motors used for the
 #define LINK_1 100
 #define LINK_2 110
 
+/**
+ * @brief       Init function for all the servos
+*/
 void init_servo(void);
-uint16_t angle_2_duty(float angle);                  // inputs: angle --> outputs: duty cycle
-void set_servo(uint16_t duty1, uint16_t duty2);         // inputs: both of the duty cycles
+
+/**
+ * @brief       Convert the angle (radians) to duty cycle
+ * @param       angle The angle that should be converted
+ * @return      corrispondent duty cycle
+*/
+uint16_t angle_2_duty(float angle);
+
+/**
+ * @brief       Set the pwms duty cycles in order to move the pen
+ * @param       duty1 Duty cycle for the first servo
+ * @param       duty2 Duty cycle for the second servo
+*/
+void set_servo(uint16_t duty1, uint16_t duty2);
+
+/**
+ * @brief       Change the pen status when it's called, high or low
+*/
 void set_pen();
-void set_position(pos_t pos);                           // inputs: position
+
+/**
+ * @brief       Overall function, receive positions and set motors
+ * @param       pos pos_t instance that contains the new position of the pen
+*/
+void set_position(pos_t pos);
 
 #endif
