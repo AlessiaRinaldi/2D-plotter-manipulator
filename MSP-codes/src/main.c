2bae335a08b96b9be0590b825698f95c61a2173c
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2024-01-17 15:01:43 +0100

    communication: test

--- MSP-codes/src/main.c
+++ MSP-codes/src/main.c
@@ -1,131 +1,21 @@
 #include "msp.h"
-//#include "motors.h"
+#include "motors.h"
 #include <driverlib.h>
 #include "stdio.h"
 #include "math.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include "inithw.h"
-//#include "motors.h"
-/*
-bool count_xy = 0; // 0=x 1=y
-uint8_t RXData = 0;
-//config struct compare configuration (PWM)
-extern Timer_A_CompareModeConfig compareConfig_PWM ;
-extern const Timer_A_UpModeConfig upConfig;
-const eUSCI_UART_ConfigV1 uartConfig =
-{
-        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-        13,                                      // BRDIV = 13
-        0,                                       // UCxBRF = 0
-        37,                                      // UCxBRS = 37
-        EUSCI_A_UART_NO_PARITY,                  // No Parity
-        EUSCI_A_UART_MSB_FIRST,                  // MSB First
-        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
-        EUSCI_A_UART_MODE,                       // UART mode
-        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
-        EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
-};
-int main(void)
-{
-    inithw();
-    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);//abilita il led sulla scheda
-    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P3, GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
-    // Setting DCO to 24MHz (upping Vcore) -> CPU operates at 24 MHz
-    FlashCtl_setWaitState(FLASH_BANK0, 1);
-    FlashCtl_setWaitState(FLASH_BANK1, 1);
-    PCM_setCoreVoltageLevel(PCM_VCORE1);
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
-    // Configuring UART Module 
-    UART_initModule(EUSCI_A2_BASE, &uartConfig);
-    // Enable UART module 
-    UART_enableModule(EUSCI_A2_BASE);
-    // Enabling interrupts
-    UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-    Interrupt_enableInterrupt(INT_EUSCIA2);
-    while(1){
-        MAP_PCM_gotoLPM0();
-    }
-}
-//da rasp mi arriva 1 byte-> da msp devo ricere un uint8_t alla volta
-void EUSCIA2_IRQHandler(void){
-    //memorizzo lo stato dell'interrupt
-    uint32_t status = UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
-    //controllo che sia arrivato un interrupt
-    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
-        RXData = UART_receiveData(EUSCI_A2_BASE);
-        for (int i = 0; i < 10; i++) {
-            //GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0); //accendi led
-            GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0); // accendi led
-            __delay_cycles(100000); // aggiungi un ritardo di 100000 cicli (adatta secondo necessità)
-            GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0); // spegni led
-            __delay_cycles(100000); // aggiungi un ritardo di 100000 cicli (adatta secondo necessità)
-        }
-        //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-        Interrupt_disableSleepOnIsrExit();
-    }
-}
-*//*----------------------------------------------------------------------------------------
-#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
-uint8_t RXData = 0;
-const eUSCI_UART_ConfigV1 uartConfig =
-{
-    EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-    13,                                      // BRDIV = 13
-    0,                                       // UCxBRF = 0
-    37,                                      // UCxBRS = 37
-    EUSCI_A_UART_NO_PARITY,                  // No Parity
-    EUSCI_A_UART_MSB_FIRST,                  // MSB First
-    EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
-    EUSCI_A_UART_MODE,                       // UART mode
-    EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
-    EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
-};
-int main(void)
-{   // Inizializza il sistema 
-    WDT_A_holdTimer();
-    FlashCtl_setWaitState(FLASH_BANK0, 1);
-    FlashCtl_setWaitState(FLASH_BANK1, 1);
-    PCM_setCoreVoltageLevel(PCM_VCORE1);
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
-    // Configura il modulo UART 
-    UART_initModule(EUSCI_A2_BASE, &uartConfig);
-    // Abilita il modulo UART
-    UART_enableModule(EUSCI_A2_BASE);
-    // Abilita gli interrupt
-    UART_enableInterrupt(EUSCI_A2_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-    Interrupt_enableInterrupt(INT_EUSCIA2);
-    Interrupt_enableSleepOnIsrExit();
-    while (1)
-    {
-        PCM_gotoLPM0InterruptSafe();
-    }
-}
-void EUSCIA2_IRQHandler(void)//gestore dell'Interrupt per l'UART
-{
-    uint32_t status = UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
-    if (status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
-    {
-        RXData = UART_receiveData(EUSCI_A2_BASE);
-        //risposta alla Raspberry Pi con lo stesso dato ricevuto
-        UART_transmitData(EUSCI_A2_BASE, RXData);
-        Interrupt_disableSleepOnIsrExit();
-    }
-}
------------------------------------------------------------------*/
-
-
 uint8_t TXData = 1;
 uint8_t RXData = 0;
 const eUSCI_UART_ConfigV1 uartConfig =
 {
         EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-        156,                                      // BRDIV = 13
-        4,                                       // UCxBRF = 0
-        0,                                      // UCxBRS = 37
+        19,                                      // BRDIV = 13
+        8,                                       // UCxBRF = 0
+        85,                                      // UCxBRS = 37
         EUSCI_A_UART_NO_PARITY,                  // No Parity
-        EUSCI_A_UART_MSB_FIRST,                  // MSB First
+        EUSCI_A_UART_LSB_FIRST,                  // MSB First
         EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
         EUSCI_A_UART_MODE,                       // UART mode
         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
@@ -142,7 +32,7 @@ int main(void){
     FlashCtl_setWaitState(FLASH_BANK0, 1);
     FlashCtl_setWaitState(FLASH_BANK1, 1);
     PCM_setCoreVoltageLevel(PCM_VCORE1);
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_3);
     // Configuring UART Module 
     UART_initModule(EUSCI_A2_BASE, &uartConfig);
     // Enable UART module 
@@ -155,34 +45,12 @@ int main(void){
 
     GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
 
-    puts("debug print \n");
-
     while(1){
-
-        /*uint8_t tmp = RXData;
-        RXData = UART_receiveData(EU
-        
-SCI_A0_BASE);
-
-        if(RXData != tmp){
-            for(int i = 0; i < 1000; i++){
-                GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-            }
-
-            GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-        }*/
-
-        
-        UART_transmitData(EUSCI_A2_BASE, TXData);
         Interrupt_enableSleepOnIsrExit();
         PCM_gotoLPM0InterruptSafe();
         
     }
 }
-
-
-// TOCO: try to blink n times the led where n is the number that occurs from uart
-// TODO: check tx
 
 void EUSCIA2_IRQHandler(void){
     uint32_t status = UART_getEnabledInterruptStatus(EUSCI_A2_BASE);
@@ -190,47 +58,10 @@ void EUSCIA2_IRQHandler(void){
     if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG){
         //int count = 0;
         RXData = UART_receiveData(EUSCI_A2_BASE);
-        //while (count < (int)RXData){
-            if((int)RXData > 3){
-               for(int j = 0; j < 100; j++){
-                    GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
-                }
-                for(int k = 0; k < 100; k++){
-                    GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
-                } 
-            } 
-            
-            //count++;
-        //}
-        
-
-        //for(int t = 0; t < 100; t++){
-            // do nothing
-        //}
-
-        // Accendi il LED
-        
-        // Trasmetti esattamente il dato ricevuto
-        //UART_transmitData(EUSCI_A2_BASE, RXData);
-        //char responseMessage[20];
-        //snprintf(responseMessage, sizeof(responseMessage), "%u\n", RXData);
-        //UART_transmitData(EUSCI_A2_BASE, responseMessage);
-        // Trasmetti la stringa ricevuta
-
-
-        /*char responseMessage[20];
-        snprintf(responseMessage, sizeof(responseMessage), "%u\n", RXData);
-
-        int i = 0;
-        while (responseMessage[i] != '\0')
-        {
-            UART_transmitData(EUSCI_A2_BASE, responseMessage[i]);
-            i++;
+        if(RXData >= 3){
+            GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+            while(1);
         }
-        */
-
-        // Spegni il LED dopo la trasmissione
-        //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
         Interrupt_disableSleepOnIsrExit();
     }
 }
