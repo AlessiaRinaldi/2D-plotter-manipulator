ecc04f1cbec1f3afa9f2f995ec9a8a5dd7d86ddf
AlessiaRinaldi <alessia.rinaldi@studenti.unitn.it>
2024-01-21 15:03:14 +0100

    communication: fix rasp

--- rasp-communication/communication_rasp.py
+++ rasp-communication/communication_rasp.py
@@ -9,6 +9,7 @@ with open('~/2D-plotter-manipulator/Vectorization-bot/photo.json', 'r') as file:
 
 # Inizializzazione della porta seriale
 ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=0)  # rasp pi 0
+
 try:
     for sublist in data:  # per ogni sublista di data
         for numbers in sublist:  # per ogni lista di numeri in sublista
@@ -16,24 +17,20 @@ try:
             num1 = int(numbers[0])
             num2 = int(numbers[1])
             # Definisci i ritardi per num1 e num2
-            delay_num1 = 0.5
-            delay_num2 = 1.0
+            delay_num1 = 0.05
+            delay_num2 = 0.5
 
-            if  num1 == 0:
-                num1 = 1
             message0 = struct.pack('>B', num1)
             ser.write(message0)
             print(f'Inviato: {num1}')
             time.sleep(delay_num1)
 
-            if num2 == 0:
-                num2 = 1
             message1 = struct.pack('>B', num2)
             ser.write(message1)
             print(f'Inviato: {num2}')
             time.sleep(delay_num2)
         # if sublist is finished -> lift pen
-        num = 0                                       # cambiare con valore riconoscibile che non sta dentro il workspace
+        num = 255                                     # cambiare con valore riconoscibile che non sta dentro il workspace
         special_message = struct.pack('>B', num)      # 1 byte 
         ser.write(special_message)                    # scrive in seriale 
         print(f'special message: {special_message}')  # debug
