9c5ea1426b4981b3c9deecc204046db0966434a5
Leonardo Pasquato <leonardo.pasquato@gmail.com>
2024-02-16 20:04:01 +0100

    pwm pull doesn't work

--- MSP-codes/src/motors.c
+++ MSP-codes/src/motors.c
@@ -23,7 +23,7 @@ Timer_A_CompareModeConfig compareConfig_PWM = {
 const Timer_A_UpModeConfig upConfig = {
         TIMER_A_CLOCKSOURCE_SMCLK,                  // SMCLK = 3 MhZ
         TIMER_A_CLOCKSOURCE_DIVIDER_1,              // SMCLK/12 = 250 KhZ
-        10000,                                      // 40 ms tick period
+        1280,                                      // 40 ms tick period         // last edit, before was 10000 that should corresponds to 40ms
         TIMER_A_TAIE_INTERRUPT_DISABLE,             // Disable Timer interrupt
         TIMER_A_CCIE_CCR0_INTERRUPT_DISABLE,        // Disable CCR0 interrupt
         TIMER_A_DO_CLEAR                            // Clear value
@@ -62,12 +62,12 @@ void init_servo(void){
 
     // For Port 2.4
     compareConfig_PWM.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
-    compareConfig_PWM.compareValue = 3000;
+    compareConfig_PWM.compareValue = 200;
     Timer_A_initCompare(TIMER_A0_BASE, &compareConfig_PWM);
 
     // For Port 5.6
     compareConfig_PWM.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_1;
-    compareConfig_PWM.compareValue = 9000;
+    compareConfig_PWM.compareValue = 560;
     Timer_A_initCompare(TIMER_A2_BASE, &compareConfig_PWM);
 }
 
